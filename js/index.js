0a8e93c78d57b0d1fddb470edaafaead591170fa
fabiano.capobianchi <fabiano.capobianchi@agid.gov.it>
2024-03-15 15:51:11 +0100

    Impostazioni JS

--- js/index.js
+++ js/index.js
@@ -1,10 +1,10 @@
-const information = document.getElementById('info')
+window.addEventListener('DOMContentLoaded', () => { })
 
+const information = document.getElementById('info')
 information.innerText = `This app is using Chrome (v${versions.chrome()}), Node.js (v${versions.node()}), and Electron (v${versions.electron()})`
 
 const callPing = async () => {
 	const response = await window.myFunctions.ping()
 	console.log(response) // prints out 'pong'
 }
-  
-callPing()
+callPing();
